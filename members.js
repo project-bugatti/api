85d3db48a2ea9015a6ed682555906188883f8b39
Tyler <tgalske@gmail.com>
2019-03-15 23:43:23 -0400

    Updated GetMember method to include media

--- members.js
+++ members.js
@@ -5,133 +5,143 @@ const { formErrorResponse } = require('./utils');
 const uuidv1 = require('uuid/v1');
 
 module.exports.GetMembers = async (event) => {
-    let isActive;
-    try {
-        isActive = event.queryStringParameters.isActive;
-    } catch (e) {
-        isActive = true;
-    }
+  let isActive = true;
+  if (event.queryStringParameters && event.queryStringParameters.isActive) {
+    isActive = event.queryStringParameters.isActive;
+  }
 
-    try {
-        const members = await db.any('SELECT * FROM members WHERE is_active = $1', [isActive]);
-        return formSuccessResponse({members} );
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  try {
+    const members = await db.any('SELECT * FROM members WHERE is_active = $1', [isActive]);
+    return formSuccessResponse({members} );
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.GetMember = async (event) => {
-    const member_id = event.pathParameters.member_id;
-
-    const sql = "SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname, " +
-        "'nickname', m.nickname, 'phone', m.phone, 'is_active', m.is_active,'quotes', " +
-        "(SELECT json_agg(json_build_object('quote_id', q.quote_id, 'quote_text', q.quote_text)) " +
-        "FROM quotes q WHERE q.author_member_id = m.member_id )) json " +
-        "FROM members m WHERE m.member_id = $1";
-    try {
-        let member = (await db.map(sql, [member_id], a => a.json))[0];
-        // Returns an array of Member objects of size one, get the 0th element
-
-        // If member has no quotes, set quotes as empty array instead of null
-        if (!member.quotes) member.quotes = [];
-        return formSuccessResponse({member: member});
-    } catch (e) {
-        return formErrorResponse(e);
+  const member_id = event.pathParameters.member_id;
+  const sql =
+    " SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname,  " +
+    " 'nickname', m.nickname, 'phone', m.phone, 'is_active', m.is_active, 'quotes', " +
+    "   (SELECT json_agg(json_build_object('quote_id', q.quote_id, 'quote_text', q.quote_text)) " +
+    "   FROM quotes q WHERE q.author_member_id = m.member_id)," +
+    " 'media', " +
+    "   (SELECT json_agg(json_build_object('media_id', media.media_id, 'title', media.title,  " +
+    "   'description', media.description, 'file_type', media.file_type, 'media_date', media.media_date))  " +
+    "   FROM media, members_media " +
+    "   WHERE media.media_id = members_media.media_id " +
+    "   AND members_media.member_id = m.member_id " +
+    "   )" +
+    " ) json " +
+    " FROM members m WHERE m.member_id = $1";
+  try {
+    let member = await db.map(sql, [member_id], a => a.json);
+    // If no quotes or media exists, return an empty array instead of null
+    if (!member.quotes) {
+      member.quotes = [];
     }
+    if (!member.media) {
+      member.media = [];
+    }
+    // Returns an array of Quote objects of size one, so return 0th element in the array
+    return formSuccessResponse({member: member[0]});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.CreateMember = async (event) => {
-    let body;
-    try {
-        body = JSON.parse(event.body);
-    } catch (e) {
-        body = event.body;
-    }
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    body = event.body;
+  }
 
-    const firstname = body.firstname, lastname = body.lastname, phone = body.phone;
-    if (firstname == null || lastname == null || phone == null) {
-        const error = { name: 'error', detail: 'Missing a required body parameter' };
-        return formErrorResponse(error);
-    }
+  const firstname = body.firstname, lastname = body.lastname, phone = body.phone;
+  if (firstname == null || lastname == null || phone == null) {
+    const error = { name: 'error', detail: 'Missing a required body parameter' };
+    return formErrorResponse(error);
+  }
 
-    const member = {
-        member_id: uuidv1(),
-        firstname,
-        lastname,
-        nickname: body.nickname,
-        phone,
-        is_active: body.is_active || true
-    };
+  const member = {
+    member_id: uuidv1(),
+    firstname,
+    lastname,
+    nickname: body.nickname,
+    phone,
+    is_active: body.is_active || true
+  };
 
-    const sql = 'INSERT INTO members(member_id, firstname, lastname, nickname, phone, is_active) ' +
-        'VALUES( $1, $2, $3, $4, $5, $6 )';
-    try {
-        await db.none(sql, [
-            member.member_id,
-            member.firstname,
-            member.lastname,
-            member.nickname,
-            member.phone,
-            member.is_active
-        ]);
-        return formSuccessResponse({member});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const sql = 'INSERT INTO members(member_id, firstname, lastname, nickname, phone, is_active) ' +
+    'VALUES( $1, $2, $3, $4, $5, $6 )';
+  try {
+    await db.none(sql, [
+      member.member_id,
+      member.firstname,
+      member.lastname,
+      member.nickname,
+      member.phone,
+      member.is_active
+    ]);
+    return formSuccessResponse({member});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.UpdateMember = async (event) => {
-    const member_id = event.pathParameters.member_id;
-    let body;
-    try {
-        body = JSON.parse(event.body);
-    } catch (e) {
-        body = event.body;
+  const member_id = event.pathParameters.member_id;
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    body = event.body;
+  }
+
+  let newMember = {
+    firstname: body.firstname,
+    lastname: body.lastname,
+    nickname: body.nickname,
+    phone: body.phone
+  };
+
+  let oldMember = {};
+
+  // Retrieve member as it currently exists
+  let sql = 'SELECT firstname, lastname, nickname, phone FROM members WHERE member_id = $1';
+  try {
+    oldMember = await db.one(sql, member_id);
+  } catch (e) {
+    return formErrorResponse(e);
+  }
+
+  // Copy props from oldMember to newMember if they don't exist in newMember
+  for (let property in oldMember) {
+    if (newMember[property] == null) {
+      newMember[property] = oldMember[property];
     }
+  }
 
-    let newMember = {
-        firstname: body.firstname,
-        lastname: body.lastname,
-        nickname: body.nickname,
-        phone: body.phone
-    };
-
-    let oldMember = {};
-
-    // Retrieve member as it currently exists
-    let sql = 'SELECT firstname, lastname, nickname, phone FROM members WHERE member_id = $1';
-    try {
-        oldMember = await db.one(sql, member_id);
-    } catch (e) {
-        return formErrorResponse(e);
-    }
-
-    // Copy props from oldMember to newMember if they don't exist in newMember
-    for (let property in oldMember) {
-        if (newMember[property] == null) {
-            newMember[property] = oldMember[property];
-        }
-    }
-
-    sql = 'UPDATE members SET firstname = $1, lastname = $2, nickname = $3, phone = $4 WHERE member_id = $5';
-    let memberValues = Object.values(newMember); // Array of updated member values
-    memberValues.push(member_id); // Append member_id for response consistency
-    try {
-        await db.none(sql, [memberValues]);
-        return formSuccessResponse({member: newMember});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  sql = 'UPDATE members SET firstname = $1, lastname = $2, nickname = $3, phone = $4 WHERE member_id = $5';
+  let memberValues = Object.values(newMember); // Array of updated member values
+  memberValues.push(member_id); // Append member_id for response consistency
+  try {
+    await db.none(sql, [memberValues]);
+    return formSuccessResponse({member: newMember});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.ToggleMemberStatus = async (event) => {
-    const member_id = event.pathParameters.member_id;
-    const sql = 'UPDATE members SET is_active = NOT is_active WHERE member_id = $1 RETURNING is_active';
-    try {
-        const memberStatus = await db.one(sql, member_id);
-        const member = { member_id, is_active: memberStatus.is_active };
-        return formSuccessResponse({member});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const member_id = event.pathParameters.member_id;
+  const sql = 'UPDATE members SET is_active = NOT is_active WHERE member_id = $1 RETURNING is_active';
+  try {
+    const memberStatus = await db.one(sql, member_id);
+    const member = { member_id, is_active: memberStatus.is_active };
+    return formSuccessResponse({member});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
--- quotes.js
+++ quotes.js
@@ -5,159 +5,173 @@ const { formErrorResponse } = require('./utils');
 const uuidv1 = require('uuid/v1');
 
 module.exports.GetQuotes = async () => {
-    const sql = "SELECT json_build_object('quote_id', q.quote_id, 'quote_text', q.quote_text, 'member', " +
-      " (SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname, " +
-      " 'nickname', m.nickname) " +
-      " FROM members m WHERE m.member_id = q.author_member_id )) json FROM quotes q";
-    try {
-        const quotes = await db.map(sql, [], a => a.json);
-        return formSuccessResponse( {quotes});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const sql = "SELECT json_build_object('quote_id', q.quote_id, 'quote_text', q.quote_text, 'member', " +
+    " (SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname, " +
+    " 'nickname', m.nickname) " +
+    " FROM members m WHERE m.member_id = q.author_member_id )) json FROM quotes q";
+  try {
+    const quotes = await db.map(sql, [], a => a.json);
+    return formSuccessResponse( {quotes});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
+/*
+  Get quote information, its author-member information, and all media related to it
+*/
 module.exports.GetQuote = async (event) => {
-    const quote_id = event.pathParameters.quote_id;
-    const sql = "SELECT json_build_object( 'quote_id', q.quote_id, 'quote_text', q.quote_text, 'member', " +
-      "     (SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname, " +
-      "             'nickname', m.nickname, 'phone', m.phone) " +
-      "     FROM members m WHERE m.member_id = q.author_member_id " +
-      " ), 'media', " +
-      "     (SELECT json_agg(json_build_object('media_id', media.media_id, 'title', media.title," +
-      "             'description', media.description, 'media_date', media.media_date, 'file_type', media.file_type)) " +
-      "     FROM media, quotes_media, quotes " +
-      "     WHERE media.is_visible = TRUE " +
-      "     AND media.media_id = quotes_media.media_id " +
-      "     AND quotes_media.quote_id = quotes.quote_id " +
-      " )) json " +
-      " FROM quotes q WHERE q.quote_id = $1;";
-    try {
-        const quote = await db.map(sql, [quote_id], a => a.json);
-        // Returns an array of Quote objects of size one, so return 0th element in the array
-        return formSuccessResponse( {quote: quote[0]});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const quote_id = event.pathParameters.quote_id;
+  const sql =
+    " SELECT json_build_object( 'quote_id', q.quote_id, 'quote_text', q.quote_text, 'member', " +
+    "   (SELECT json_build_object('member_id', m.member_id, 'firstname', m.firstname, 'lastname', m.lastname, " +
+    "     'nickname', m.nickname, 'phone', m.phone) " +
+    "   FROM members m WHERE m.member_id = q.author_member_id " +
+    "   )," +
+    " 'media', " +
+    "   (SELECT json_agg(json_build_object('media_id', media.media_id, 'title', media.title," +
+    "       'description', media.description, 'media_date', media.media_date, 'file_type', media.file_type)) " +
+    "   FROM media, quotes_media, quotes " +
+    "   WHERE media.is_visible = TRUE " +
+    "   AND media.media_id = quotes_media.media_id " +
+    "   AND quotes_media.quote_id = quotes.quote_id " +
+    " )) json " +
+    " FROM quotes q WHERE q.quote_id = $1;";
+  try {
+    const quote = await db.map(sql, [quote_id], a => a.json);
+    // Returns an array of Quote objects of size one, so return 0th element in the array
+    return formSuccessResponse( {quote: quote[0]});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.CreateQuote = async (event) => {
-    let body;
-    try {
-        body = JSON.parse(event.body);
-    } catch (e) {
-        body = event.body;
-    }
-    if (body.quote_text == null || body.author_member_id == null) {
-        const error = { name: 'error', detail: 'Missing a required body parameter' };
-        return formErrorResponse(error);
-    }
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    body = event.body;
+  }
+  if (body.quote_text == null || body.author_member_id == null) {
+    const error = { name: 'error', detail: 'Missing a required body parameter' };
+    return formErrorResponse(error);
+  }
 
-    const quote = {
-        quote_id: uuidv1(),
-        quote_text: body.quote_text,
-        author_member_id: body.author_member_id,
-        is_visible: body.is_visible || true
-    };
-    const sql = 'INSERT INTO quotes(quote_id, quote_text, author_member_id, content_id, is_visible) ' +
-      'VALUES( $1, $2, $3, $4, $5 )';
-    try {
-        await db.none(sql, [
-            quote.quote_id,
-            quote.quote_text,
-            quote.author_member_id,
-            quote.content_id,
-            quote.is_visible
-        ]);
-        return formSuccessResponse( {quote});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const quote = {
+    quote_id: uuidv1(),
+    quote_text: body.quote_text,
+    author_member_id: body.author_member_id,
+    is_visible: body.is_visible || true
+  };
+  const sql = 'INSERT INTO quotes(quote_id, quote_text, author_member_id, content_id, is_visible) ' +
+    'VALUES( $1, $2, $3, $4, $5 )';
+  try {
+    await db.none(sql, [
+      quote.quote_id,
+      quote.quote_text,
+      quote.author_member_id,
+      quote.content_id,
+      quote.is_visible
+    ]);
+    return formSuccessResponse( {quote});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
+/*
+  Change a quote's author or text
+*/
 module.exports.UpdateQuote = async (event) => {
-    const quote_id = event.pathParameters.quote_id;
-    let body;
-    try {
-        body = JSON.parse(event.body);
-    } catch (e) {
-        body = event.body;
-    }
+  const quote_id = event.pathParameters.quote_id;
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (e) {
+    body = event.body;
+  }
 
-    let newQuote = {
-        quote_text: body.quote_text,
-        author_member_id: body.author_member_id
-    };
+  let newQuote = {
+    quote_text: body.quote_text,
+    author_member_id: body.author_member_id
+  };
 
-    // Retrieve quote as it currently exists
-    let oldQuote = {};
-    let sql = 'SELECT quote_text, author_member_id, content_id FROM quotes WHERE quote_id = $1';
-    try {
-        oldQuote = await db.one(sql, [quote_id]);
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  // Retrieve quote as it currently exists
+  let oldQuote = {};
+  let sql = 'SELECT quote_text, author_member_id, content_id FROM quotes WHERE quote_id = $1';
+  try {
+    oldQuote = await db.one(sql, [quote_id]);
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 
-    // Copy props from oldQuote to newQuote if they don't exist in newQuote
-    for (let property in oldQuote) {
-        if (newQuote[property] == null) {
-            newQuote[property] = oldQuote[property];
-        }
+  // Copy props from oldQuote to newQuote if they don't exist in newQuote
+  for (let property in oldQuote) {
+    if (newQuote[property] == null) {
+      newQuote[property] = oldQuote[property];
     }
+  }
 
-    sql = 'UPDATE quotes SET quote_text = $1, author_member_id = $2';
-    let quoteValues = Object.values(newQuote); // Array of updated quote values
-    quoteValues.push(quote_id); // Append quote_id for response consistency
-    try {
-        await db.none(sql, quoteValues);
-        return formSuccessResponse({quote: newQuote});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  sql = 'UPDATE quotes SET quote_text = $1, author_member_id = $2';
+  let quoteValues = Object.values(newQuote); // Array of updated quote values
+  quoteValues.push(quote_id); // Append quote_id for response consistency
+  try {
+    await db.none(sql, quoteValues);
+    return formSuccessResponse({quote: newQuote});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
+/*
+  Link a media item to a quote
+*/
 module.exports.JoinQuoteMedia = async (event) => {
-    const quote_id = event.pathParameters.quote_id;
-    const media_id = event.pathParameters.media_id;
+  const quote_id = event.pathParameters.quote_id;
+  const media_id = event.pathParameters.media_id;
 
-    const sql = 'INSERT INTO quotes_media VALUES($1, $2)';
-    try {
-        await db.none(sql, [quote_id, media_id]);
-        return formSuccessResponse();
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const sql = 'INSERT INTO quotes_media VALUES($1, $2)';
+  try {
+    await db.none(sql, [quote_id, media_id]);
+    return formSuccessResponse();
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
+/*
+  Remove the link between a quote and media item
+*/
 module.exports.RemoveMediaFromQuote = async (event) => {
-    const quote_id = event.pathParameters.quote_id;
-    const media_id = event.pathParameters.media_id;
+  const quote_id = event.pathParameters.quote_id;
+  const media_id = event.pathParameters.media_id;
 
-    const sql = 'DELETE FROM quotes_media WHERE quote_id = $1 AND media_id = $2';
-    try {
-        const result = await db.result(sql, [quote_id, media_id]);
-        if (result.rowCount === 1) {
-            return formSuccessResponse();
-        } else {
-            const error = {
-                message: 'Combination of quote and media item does not exist',
-                detail: 'Delete query returned zero rows affected'
-            };
-            return formErrorResponse(error);
-        }
-    } catch (e) {
-        return formErrorResponse(e);
+  const sql = 'DELETE FROM quotes_media WHERE quote_id = $1 AND media_id = $2';
+  try {
+    const result = await db.result(sql, [quote_id, media_id]);
+    if (result.rowCount === 1) {
+      return formSuccessResponse();
+    } else {
+      const error = {
+        message: 'Combination of quote and media item does not exist',
+        detail: 'Delete query returned zero rows affected'
+      };
+      return formErrorResponse(error);
     }
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
 
 module.exports.ToggleQuoteVisibility = async (event) => {
-    const quote_id = event.pathParameters.quote_id;
-    const sql = 'UPDATE quotes SET is_visible = NOT is_visible WHERE quote_id = $1 RETURNING is_visible';
-    try {
-        const quoteVisiblity = await db.one(sql, [quote_id]);
-        const quote = { quote_id, is_visible: quoteVisiblity };
-        return formSuccessResponse({quote});
-    } catch (e) {
-        return formErrorResponse(e);
-    }
+  const quote_id = event.pathParameters.quote_id;
+  const sql = 'UPDATE quotes SET is_visible = NOT is_visible WHERE quote_id = $1 RETURNING is_visible';
+  try {
+    const quoteVisiblity = await db.one(sql, [quote_id]);
+    const quote = { quote_id, is_visible: quoteVisiblity };
+    return formSuccessResponse({quote});
+  } catch (e) {
+    return formErrorResponse(e);
+  }
 };
